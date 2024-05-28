c98ec5169d6d2fc161879c83a1cee54bdd4500a6
axl2301 <126207147+axl2301@users.noreply.github.com>
2024-05-28 11:38:39 -0600

    feat: moved admin_decrypt_page to encrypt.py

--- pages/Buscar_en_DB.py
+++ pages/Buscar_en_DB.py
@@ -5,11 +5,10 @@ import pyotp
 from dotenv import load_dotenv
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from back.encrypt import decrypt_data
-from back.model import SessionLocal, General, Transit, Health, Education
 from sqlalchemy.orm import joinedload
 import pandas as pd
 # our decrypt data function
-from back.encrypt import decrypt_data, decrypt_large_data
+from back.encrypt import decrypt_data, decrypt_large_data, admin_decrypt_page
 
 st.set_page_config(page_title='Casa Monarca', page_icon=':butterfly:')
 
@@ -19,102 +18,6 @@ if st.session_state.get('authenticated'):
     if st.sidebar.button("Cerrar sesión"):
         # Al hacer clic en cerrar sesión, cambiamos el estado a no autenticado
         st.session_state.authenticated = False
-
-# función para desencriptar todos los dataos guardados en la base de datos
-def admin_decrypt_page(private_key):
-    with SessionLocal() as session:
-        general_query = session.query(General).all()
-        transit_query = {transit.id: transit for transit in session.query(Transit).all()}
-        health_query = {health.id: health for health in session.query(Health).all()}
-        education_query = {education.id: education for education in session.query(Education).all()}
-
-        data = []
-        for migrant in general_query:
-            transit = transit_query.get(migrant.id, {})
-            health = health_query.get(migrant.id, {})
-            education = education_query.get(migrant.id, {})
-            
-            decrypted_migrant = {
-                'id': migrant.id,
-                'arrival_date': decrypt_data(private_key, migrant.arrival_date),
-                'type': decrypt_data(private_key, migrant.type),
-                'name': decrypt_data(private_key, migrant.name),
-                'last_name': decrypt_data(private_key, migrant.last_name),
-                'gender': decrypt_data(private_key, migrant.gender),
-                'birth_date': decrypt_data(private_key, migrant.birth_date),
-                'age': decrypt_data(private_key, migrant.age),
-                'country_of_origin': decrypt_data(private_key, migrant.country_of_origin),
-                'civil_status': decrypt_data(private_key, migrant.civil_status),
-                'has_children': decrypt_data(private_key, migrant.has_children),
-                'children_traveling': decrypt_data(private_key, migrant.children_traveling),
-                'can_return_to_country': decrypt_data(private_key, migrant.can_return_to_country),
-                'reason_cannot_return': decrypt_data(private_key, migrant.reason_cannot_return),
-                'access_to_casa_monarca': decrypt_data(private_key, migrant.access_to_casa_monarca),
-                'reason_for_denial': decrypt_data(private_key, migrant.reason_for_denial),
-                'services_provided': decrypt_data(private_key, migrant.services_provided),
-                'assigned_dormitory': decrypt_data(private_key, migrant.assigned_dormitory),
-                'distinctive_signs': decrypt_data(private_key, migrant.distinctive_signs),
-                'emergency_contact': decrypt_data(private_key, migrant.emergency_contact),
-                'emergency_contact_location': decrypt_data(private_key, migrant.emergency_contact_location),
-                'final_observations': decrypt_data(private_key, migrant.final_observations),
-                'front_photo': decrypt_large_data(private_key, migrant.front_photo) if migrant.front_photo else None,
-                'right_profile_photo': decrypt_large_data(private_key, migrant.right_profile_photo) if migrant.right_profile_photo else None,
-                'left_profile_photo': decrypt_large_data(private_key, migrant.left_profile_photo) if migrant.left_profile_photo else None,
-                'transit': {
-                    'date_left_origin': decrypt_data(private_key, transit.date_left_origin),
-                    'traveling_alone_accompanied': decrypt_data(private_key, transit.traveling_alone_accompanied),
-                    'who_accompanied': decrypt_data(private_key, transit.who_accompanied),
-                    'which_relative': decrypt_data(private_key, transit.which_relative),
-                    'how_traveled': decrypt_data(private_key, transit.how_traveled),
-                    'reason_for_leaving': decrypt_data(private_key, transit.reason_for_leaving),
-                    'abuse_during_travel': decrypt_data(private_key, transit.abuse_during_travel),
-                    'type_of_abuse': decrypt_data(private_key, transit.type_of_abuse),
-                    'abuse_in_mexico': decrypt_data(private_key, transit.abuse_in_mexico),
-                    'type_of_abuse_mexico': decrypt_data(private_key, transit.type_of_abuse_mexico),
-                    'abuser': decrypt_data(private_key, transit.abuser),
-                    'paid_guide': decrypt_data(private_key, transit.paid_guide),
-                    'amount_paid': decrypt_data(private_key, transit.amount_paid),
-                    'date_entered_mexico': decrypt_data(private_key, transit.date_entered_mexico),
-                    'entry_point_mexico': decrypt_data(private_key, transit.entry_point_mexico),
-                    'final_destination': decrypt_data(private_key, transit.final_destination),
-                    'destination_monterrey': decrypt_data(private_key, transit.destination_monterrey),
-                    'reason_stay_monterrey': decrypt_data(private_key, transit.reason_stay_monterrey),
-                    'support_network_monterrey': decrypt_data(private_key, transit.support_network_monterrey),
-                    'time_knowing_support': decrypt_data(private_key, transit.time_knowing_support),
-                    'tried_enter_us': decrypt_data(private_key, transit.tried_enter_us),
-                    'support_network_us': decrypt_data(private_key, transit.support_network_us),
-                    'description_support_us': decrypt_data(private_key, transit.description_support_us),
-                    'been_in_migration_station': decrypt_data(private_key, transit.been_in_migration_station),
-                    'suffered_aggression': decrypt_data(private_key, transit.suffered_aggression),
-                    'migration_station_location': decrypt_data(private_key, transit.migration_station_location),
-                    'description_of_facts': decrypt_data(private_key, transit.description_of_facts),
-                    'filed_complaint': decrypt_data(private_key, transit.filed_complaint),
-                    'reason_no_complaint': decrypt_data(private_key, transit.reason_no_complaint),
-                    'solution_offered': decrypt_data(private_key, transit.solution_offered),
-                    'stayed_in_shelter': decrypt_data(private_key, transit.stayed_in_shelter),
-                    'last_shelter': decrypt_data(private_key, transit.last_shelter),
-                },
-                'health': {
-                    'has_illness': decrypt_data(private_key, health.has_illness),
-                    'illness_details': decrypt_data(private_key, health.illness_details),
-                    'on_medical_treatment': decrypt_data(private_key, health.on_medical_treatment),
-                    'medical_treatment_description': decrypt_data(private_key, health.medical_treatment_description),
-                    'has_allergy': decrypt_data(private_key, health.has_allergy),
-                    'allergy_details': decrypt_data(private_key, health.allergy_details),
-                    'is_pregnant': decrypt_data(private_key, health.is_pregnant),
-                    'months_pregnant': decrypt_data(private_key, health.months_pregnant),
-                    'has_prenatal_care': decrypt_data(private_key, health.has_prenatal_care),
-                },
-                'education': {
-                    'can_read_write': decrypt_data(private_key, education.can_read_write),
-                    'last_grade_study': decrypt_data(private_key, education.last_grade_study),
-                    'languages_spoken': decrypt_data(private_key, education.languages_spoken),
-                    'other_language': decrypt_data(private_key, education.other_language),
-                }
-            }
-            data.append(decrypted_migrant)
-
-        return data
 
 
 # Iniciamos la página
