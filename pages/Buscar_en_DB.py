9ee59992950612a657ef7e3bdfcab47dbe7c6d50
axl2301 <126207147+axl2301@users.noreply.github.com>
2024-05-29 18:42:56 -0600

    feat: added minor details

--- pages/Buscar_en_DB.py
+++ pages/Buscar_en_DB.py
@@ -22,7 +22,10 @@ if st.session_state.get('authenticated'):
     st.sidebar.write(f"Permisos: {st.session_state['user_type']}")
     if st.sidebar.button("Cerrar sesión"):
         # Al hacer clic en cerrar sesión, cambiamos el estado a no autenticado
-        st.session_state.authenticated = False
+        st.session_state.authenticated = False 
+        # también reiniciamos el estado de la otp, para que tenga que volverla a 
+        # poner en caso de iniciar sesión nuevamente
+        st.session_state.otp_verified = False 
 
 
 # Iniciamos la página
@@ -37,7 +40,7 @@ if __name__ == "__main__":
         if not st.session_state['otp_verified']:
 
             st.markdown("""
-                #### Bienvenido a la sección de administración. Esta página está diseñada exclusivamente para el acceso y la gestión por parte de administradores autorizados.
+                #### Bienvenid@ a la sección de administración. Esta página está diseñada exclusivamente para el acceso y la gestión por parte de administradores autorizados.
 
                 ## Información Sensible
 
@@ -93,6 +96,23 @@ if __name__ == "__main__":
             # Agregar un menú para seleccionar el formato de búsqueda
             # Filtrar los migrantes según el estado actual o pasado
             if 'consult' not in st.session_state:
+                st.info("En esta sección podrás consultar información sobre todos los migrantes que han pasado por Casa Monarca.")
+                st.markdown("""
+                                            
+                ### Migrantes actuales:
+                Aquí podrás consultar información sobre todos los migrantes que actualmente están en Casa Monarca.
+                Si es necesario, podrás dar de baja a cualquier migrante proporcionando una razón para ello.
+                                
+                ### Migrantes pasados:
+                Aquí podrás consultar información sobre todos los migrantes que estuvieron en Casa Monarca por un tiempo.
+
+                ### Filtros de selección:
+                Podrás filtrar la búsqueda mediante dos opciones: nombre completo o un rango de fechas a consultar.
+                            
+                **Una vez que hayas seleccionado a la persona de interés, haz click en el botón 'Consultar información'.
+                Cuando se haya finalizado la consulta o se quiera consultar a otro migrante, haz click en el botón 'Finalizar consulta'.**
+                """)
+
                 migrant_type = st.selectbox('Seleccione el tipo de migrantes', ['Actuales', 'Pasados'])
                 st.session_state['migrant_type'] = migrant_type  # Guarda el tipo de migrante seleccionado
                 
@@ -109,6 +129,11 @@ if __name__ == "__main__":
                 if search_type == 'Nombre completo':
                     all_names = df_filtered['full_name'].unique()
                     selected_migrant = st.selectbox('Seleccione un nombre', ['Seleccione'] + list(all_names))
+                    dummy_df = df[(df['full_name'] == selected_migrant)]
+                    try: 
+                        st.image(base64.b64decode(dummy_df['front_photo'].values[0]), caption='Foto de frente')
+                    except IndexError:
+                        st.write(" ")
                     if selected_migrant != 'Seleccione':
                         ids_for_name = df_filtered[df_filtered['full_name'] == selected_migrant]['id'].unique()
                         if len(ids_for_name) > 1:
@@ -119,19 +144,31 @@ if __name__ == "__main__":
 
                 elif search_type in ['Fecha de registro', 'Fecha de salida']:
                     if migrant_type == 'Actuales':
-                        unique_dates = df_filtered['arrival_date'].unique()
+                        min_date = pd.to_datetime(df_filtered['arrival_date'].min()).date()
+                        max_date = pd.to_datetime(df_filtered['arrival_date'].max()).date()
                     else:
-                        unique_dates = df_filtered['date_departure'].unique()
-                    selected_date = st.selectbox('Seleccione una fecha', ['Seleccione'] + list(unique_dates))
-                    if selected_date != 'Seleccione':
+                        df_filtered['date_departure'] = pd.to_datetime(df_filtered['date_departure']).dt.date
+                        min_date = pd.to_datetime(df_filtered['date_departure'].min()).date()
+                        max_date = pd.to_datetime(df_filtered['date_departure'].max()).date()
+                    
+                    date_range = st.date_input('Seleccione un rango de fechas', [min_date, max_date], min_value=min_date, max_value=max_date)
+
+                    if date_range:
+                        start_date, end_date = date_range
                         if migrant_type == 'Actuales':
-                            filtered_df = df_filtered[df_filtered['arrival_date'] == selected_date]
+                            filtered_df = df_filtered[(df_filtered['arrival_date'] >= start_date) & (df_filtered['arrival_date'] <= end_date)]
                         else:
-                            filtered_df = df_filtered[df_filtered['date_departure'] == selected_date]
+                            filtered_df = df_filtered[(df_filtered['date_departure'] >= start_date) & (df_filtered['date_departure'] <= end_date)]
+
                         selected_name = st.selectbox('Seleccione un nombre', ['Seleccione'] + filtered_df['full_name'].tolist())
                         if selected_name != 'Seleccione':
                             selected_id = filtered_df[filtered_df['full_name'] == selected_name]['id'].values[0]
                             selected_migrant = selected_name
+                            dummy_df = df[(df['full_name'] == selected_migrant)]
+                            try: 
+                                st.image(base64.b64decode(dummy_df['front_photo'].values[0]), caption='Foto de frente')
+                            except IndexError:
+                                st.write(" ")
 
                 if selected_migrant and selected_id and st.button('Consultar información', key='consultar_info'):
                     st.session_state['selected_migrant'] = selected_migrant
@@ -140,6 +177,18 @@ if __name__ == "__main__":
                     st.rerun()
 
             if st.session_state.get('consult'):
+                st.markdown("""
+                    ### Consulta de Información
+
+                    En esta sección podrás consultar información detallada sobre los migrantes en cuatro rubros diferentes:
+
+                    * **General**: Aquí encontrarás información básica y demográfica de los migrantes, incluyendo datos personales y de contacto.
+                    * **Educación**: En esta categoría se recopila información sobre el nivel educativo, habilidades lingüísticas y formación académica de los migrantes.
+                    * **Salud**: Este apartado incluye datos relacionados con el estado de salud, historial médico, tratamientos actuales y cualquier condición médica relevante.
+                    * **Tránsito**: Aquí se detallan los datos sobre el tránsito del migrante, incluyendo rutas de viaje, medios de transporte, y experiencias durante el trayecto.
+
+                    Utiliza los filtros disponibles para encontrar y consultar la información específica que necesitas.
+                    """)
                 selected_id = st.session_state['selected_id']
                 selected_migrant = st.session_state['selected_migrant']
                 migrant_type = st.session_state['migrant_type']  # Recupera el tipo de migrante seleccionado
