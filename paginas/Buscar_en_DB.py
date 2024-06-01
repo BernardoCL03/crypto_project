58c2f479e21748134a98206266a3de0e06f0b88a
axl2301 <126207147+axl2301@users.noreply.github.com>
2024-05-31 18:06:16 -0600

    chore: added the option to add services in case migrant is a refugee

--- paginas/Buscar_en_DB.py
+++ paginas/Buscar_en_DB.py
@@ -6,14 +6,12 @@ import time
 from dotenv import load_dotenv
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
-from back.encrypt import decrypt_data
-from sqlalchemy.orm import joinedload
 import pandas as pd
 import datetime
-from datetime import datetime, date
+from datetime import datetime
 # our decrypt data function
 from back.model import SessionLocal, General, Logs # table names
-from back.encrypt import decrypt_data, decrypt_large_data, admin_decrypt_page, encrypt_data
+from back.encrypt import admin_decrypt_page, encrypt_data, encrypt_large_data
 
 #st.set_page_config(page_title='Casa Monarca', page_icon=':butterfly:')
 
@@ -39,7 +37,7 @@ def buscar_en_db_page():
 
                     ### Acceso Restringido
 
-                    ** Si usted está aquí, es porque ha sido verificado y autorizado para manejar dichos datos. Le pedimos proceder con la máxima precaución y responsabilidad. **
+                    **Si usted está aquí, es porque ha sido verificado y autorizado para manejar dichos datos. Le pedimos proceder con la máxima precaución y responsabilidad.**
 
                     ### Responsabilidad
 
@@ -197,9 +195,35 @@ def buscar_en_db_page():
                     selected_migrant = st.session_state['selected_migrant']
                     migrant_type = st.session_state['migrant_type']  # Recupera el tipo de migrante seleccionado
                     st.info(f'Consulta de información sobre {selected_migrant}')
+                    # Verificar la condición de refugiado
                     df_migrant = df[(df['full_name'] == selected_migrant) & (df['id'] == selected_id)]
-                    st.image(base64.b64decode(df_migrant['front_photo'].values[0]), caption='Foto de frente')
-                    data_type = st.selectbox('Seleccione el tipo de dato', ['Seleccione', 'General', 'Health', 'Transit', 'Education'])
+                    # Verificar la condición de refugiado
+                    if df_migrant['refugee'].values[0] == 'Sí':
+                        st.markdown("""
+                            ### Acciones para Migrantes en Condición de Refugiado
+
+                            Si el migrante se encuentra en condición de refugiado, puedes realizar las siguientes acciones adicionales:
+
+                            * **Actualizar Servicios Proporcionados**: Modifica y actualiza la lista de servicios que han sido proporcionados al migrante.
+                        """)
+                    if df_migrant['refugee'].values[0] == 'Sí':
+                        st.info("**Condición:** Refugiado")
+                    else:
+                        st.info("**Condición:** En tránsito")
+                    # Crear tres columnas
+                    col1, col2, col3 = st.columns(3)
+
+                    # Mostrar las imágenes en las columnas correspondientes
+                    with col1:
+                        st.image(base64.b64decode(df_migrant['front_photo'].values[0]), caption='Foto de frente')
+
+                    with col2:
+                        st.image(base64.b64decode(df_migrant['right_profile_photo'].values[0]), caption='Foto de perfil derecho')
+
+                    with col3:
+                        st.image(base64.b64decode(df_migrant['left_profile_photo'].values[0]), caption='Foto de perfil izquierdo')
+                    st.markdown("### Seleccione el tipo de dato a Consultar")
+                    data_type = st.selectbox('Consulta sobre',['Seleccione', 'General', 'Health', 'Transit', 'Education'])
 
                     if data_type != 'Seleccione':
                         if data_type == 'General':
@@ -208,6 +232,9 @@ def buscar_en_db_page():
                             columnas.remove('front_photo')
                             columnas.remove('right_profile_photo')
                             columnas.remove('left_profile_photo')
+                            if migrant_type == 'Actuales':
+                                columnas.remove('date_departure') 
+                                columnas.remove('reason_departure')
                         elif data_type == 'Health':
                             columnas = [col for col in df_migrant.columns if 'health' in col]
                         elif data_type == 'Transit':
@@ -220,19 +247,90 @@ def buscar_en_db_page():
                         df_transpuesto.columns = [selected_migrant]
                         st.dataframe(df_transpuesto, width=1200, height=600)
 
-                        st.image(base64.b64decode(df_migrant['right_profile_photo'].values[0]), caption='Foto de perfil derecho')
-                        st.image(base64.b64decode(df_migrant['left_profile_photo'].values[0]), caption='Foto de perfil izquierdo')
+                    # Bloque para actualizar los servicios proporcionados a un migrante refugiado
+                    if df_migrant['refugee'].values[0] == 'Sí' and migrant_type == "Actuales":
+                        st.markdown("### Actualización de Servicios Proporcionados")
 
-                    if st.button('Finalizar consulta', key='finalizar_consulta'):
-                        del st.session_state['selected_migrant']
-                        del st.session_state['selected_id']
-                        del st.session_state['consult']
-                        del st.session_state['migrant_type']  # Elimina el tipo de migrante seleccionado
-                        st.rerun()
+                        # Servicios que se ofrecen a personas con solicitud/condición de refugiado
+                        services_refugee = [
+                            'Orientaciones Legales',
+                            'Asesorías legales',
+                            'Trámite CURP',
+                            'Obtención RFC',
+                            'Obtención NSS',
+                            'Afiliación IMSS',
+                            'Acceso a vivienda',
+                            'Apertura Cta. Bancaria',
+                            'Atención médica (Acompañamiento)',
+                            'Revalidación de estudios (Acompañamiento)',
+                            'Vinculación Educativa (Escolaridad obligatoria)',
+                            'Capacitación técnica certificada (acompañamiento)',
+                            'Canalización para recepción de apoyo para la capacitación',
+                            'Capacitación / Orientación para ingresar al mercado laboral',
+                            'Canalización a empresa u organización especializada para la vinculación laboral',
+                            'Canalización a una empresa',
+                            'Canalización al SNE',
+                            'Canalización a una bolsa de empleo',
+                            'Canalización a organización especializada para el empleo',
+                            'Necesidad específica de protección',
+                            'Recibieron atención psicológica y psicosocial fuera del albergue',
+                            'Identificación de necesidades psicosocial, orientación y canalización',
+                            'Acompañamiento para acceso a servicios',
+                            'Primeros auxilios psicológicos',
+                            'Psicoterapia (individual/familiar)',
+                            'Psicoterapia Grupal',
+                            'Talleres psicoeducativos',
+                            'Canalización a servicios de psiquiatría'
+                        ]
+
+                        # Servicios ya proporcionados
+                        existing_services = df_migrant['services_provided'].values[0].split(', ') if 'services_provided' in df_migrant.columns else []
+
+                        # Filtrar servicios ya proporcionados para no mostrarlos en el multiselect
+                        available_services = [service for service in services_refugee if service not in existing_services]
+
+                        # Formulario para actualizar servicios proporcionados
+                        with st.form(key='update_services_form'):
+                            new_services_provided = st.multiselect(
+                                "¿Cuáles servicios se le brindaron a la persona?",
+                                options=available_services
+                            )
+                            confirmar_servicios = st.form_submit_button('Actualizar Servicios')
+
+                            if confirmar_servicios:
+                                # Unir los nuevos servicios con los ya existentes
+                                all_services_provided = existing_services + new_services_provided
+
+                                with SessionLocal() as session:
+                                    migrant = session.query(General).filter(General.id == int(selected_id)).first()
+                                    if migrant:
+                                        public_key_base64 = os.getenv('PUBLIC_KEY')
+                                        public_key_pem = base64.b64decode(public_key_base64)
+                                        PUBLIC_KEY = load_pem_public_key(public_key_pem)
+                                        services_str = ', '.join(all_services_provided)
+                                        migrant.services_provided = encrypt_data(PUBLIC_KEY,services_str)
+                                        
+                                        # Registrar en los logs la actualización de servicios
+                                        log_entry = Logs(
+                                            action="Migrant information",
+                                            user_name=st.session_state['username'],
+                                            user_type=st.session_state['user_type'],
+                                            description=f"Usuario '{st.session_state['username']}' con ID '{st.session_state['id']}' actualizó los servicios proporcionados a migrante: '{selected_migrant}'."
+                                        )
+                                        session.add(log_entry)
+                                        session.commit()
+
+                                    st.success('Servicios proporcionados actualizados exitosamente.')
+                                    del st.session_state['selected_migrant']
+                                    del st.session_state['selected_id']
+                                    del st.session_state['consult']
+                                    del st.session_state['migrant_type']
+                                    time.sleep(1)
+                                    st.rerun()
 
                     if migrant_type == 'Actuales':
+                        st.markdown("### Dar de baja")
                         with st.form(key='dar_de_baja_form'):
-                            st.write('Dar de baja:')
                             reason_departure = st.text_input('Razón de baja')
                             today = datetime.today().date()
                             arrival_date = df_migrant['arrival_date'].values[0]
@@ -273,6 +371,12 @@ def buscar_en_db_page():
                                 else:
                                     st.error('Por favor, proporciona una razón de baja.')
 
+                    if st.button('Finalizar consulta', key='finalizar_consulta'):
+                        del st.session_state['selected_migrant']
+                        del st.session_state['selected_id']
+                        del st.session_state['consult']
+                        del st.session_state['migrant_type']  # Elimina el tipo de migrante seleccionado
+                        st.rerun()
         
         elif st.session_state.get('authenticated') and st.session_state['user_type'] == 'Colaborador':
             st.error('No tienes permisos para acceder a esta página.')
