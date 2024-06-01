88264cd988fa35d24376d512bd94b184aed10376
axl2301 <126207147+axl2301@users.noreply.github.com>
2024-05-31 18:06:28 -0600

    feat: enhanced visualization

--- paginas/Registro_de_Logs.py
+++ paginas/Registro_de_Logs.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 from back.model import SessionLocal, Logs
 
-#st.set_page_config(page_title='Casa Monarca', page_icon=':butterfly:')
-
 def ver_logs():
-
     # Título para la sección de visualización
     st.title('Logs Registrados')
 
@@ -13,64 +10,76 @@ def ver_logs():
     session = SessionLocal()
 
     if st.session_state.get('authenticated'):
-            if st.session_state['user_type'] == 'Admin':
-                st.markdown("""
-                    En esta página puedes consultar los registros de actividad en el sistema. Utiliza los filtros disponibles para encontrar información específica de manera eficiente.
+        if st.session_state['user_type'] == 'Admin':
+            st.markdown("""
+                En esta página puedes consultar los registros de actividad en el sistema. Utiliza los filtros disponibles para encontrar información específica de manera eficiente.
 
-                    ### Funcionalidades:
+                ### Funcionalidades:
 
-                    - **Filtrar por Username**: Puedes seleccionar un usuario específico para ver únicamente sus actividades registradas.
-                    - **Filtrar por Privilegios**: Filtra las actividades según los privilegios del usuario, como Admin, User o Colaborador.
-                    - **Filtrar por Acción**: Selecciona un tipo de acción específica para ver todas las instancias en las que se realizó esa acción.
-                    - **Filtrar por Rango de Fechas**: Selecciona un rango de fechas para ver las actividades registradas durante ese período.
+                - **Filtrar por Username**: Puedes seleccionar un usuario específico para ver únicamente sus actividades registradas.
+                - **Filtrar por Privilegios**: Filtra las actividades según los privilegios del usuario, como Admin, User o Colaborador.
+                - **Filtrar por Acción**: Selecciona un tipo de acción específica para ver todas las instancias en las que se realizó esa acción.
+                - **Filtrar por Rango de Fechas**: Selecciona un rango de fechas para ver las actividades registradas durante ese período.
 
-                    ### Cómo Usar los Filtros:
+                ### Cómo Usar los Filtros:
 
-                    1. Haz clic en el botón "Agregar filtro" para añadir un nuevo filtro.
-                    2. Selecciona la clave del filtro (Username, Privilegios, Acción, Timestamp).
-                    3. Dependiendo de la clave seleccionada, elige el valor específico o el rango de fechas.
-                    4. Repite los pasos anteriores para agregar múltiples filtros.
-                    5. Haz clic en el botón "Aplicar filtros" para ver los registros filtrados.
+                1. Haz clic en el botón "Agregar filtro" para añadir un nuevo filtro.
+                2. Selecciona la clave del filtro (Username, Privilegios, Acción, Timestamp).
+                3. Dependiendo de la clave seleccionada, elige el valor específico o el rango de fechas.
+                4. Repite los pasos anteriores para agregar múltiples filtros.
+                5. Haz clic en el botón "Aplicar filtros" para ver los registros filtrados.
 
-                    > **Nota**: Puedes combinar múltiples filtros para obtener resultados más precisos. Por ejemplo, puedes filtrar por un usuario específico y un rango de fechas para ver sus actividades durante un período determinado.
+                > **Nota**: Puedes combinar múltiples filtros para obtener resultados más precisos. Por ejemplo, puedes filtrar por un usuario específico y un rango de fechas para ver sus actividades durante un período determinado.
 
-                    ### Importancia de los Logs:
+                ### Importancia de los Logs:
 
-                    Los logs del sistema son fundamentales para monitorear la actividad, identificar problemas y garantizar la seguridad de la información. Utiliza esta herramienta para mantener un seguimiento detallado y preciso de todas las operaciones realizadas en el sistema.
-                """)
+                Los logs del sistema son fundamentales para monitorear la actividad, identificar problemas y garantizar la seguridad de la información. Utiliza esta herramienta para mantener un seguimiento detallado y preciso de todas las operaciones realizadas en el sistema.
+            """)
 
-                try:
-                    # Obtener todos los usuarios
-                    logs = session.query(Logs).all()
-                    if logs:
-                        # Preparar los datos para la tabla
-                        log_info = [[log.id, log.user_name, log.user_type, log.action, log.description, log.timestamp] for log in logs]
-                        # Crear un DataFrame con pandas y especificar los nombres de las columnas
-                        df = pd.DataFrame(log_info, columns=['Log_ID', 'Username', 'Privilegios', 'Acción', "Descripción", "Timestamp"])
-                        variables = ['Username', 'Privilegios', 'Acción', 'Timestamp']
-                        dictFilters = {}
-                        for column in df.columns.tolist():
-                            if column in variables:
-                                dictFilters[column] = list(df[column].unique())
+            try:
+                # Obtener todos los logs
+                logs = session.query(Logs).all()
+                if logs:
+                    # Preparar los datos para la tabla
+                    log_info = [[log.id, log.user_name, log.user_type, log.action, log.description, log.timestamp] for log in logs]
+                    # Crear un DataFrame con pandas y especificar los nombres de las columnas
+                    df = pd.DataFrame(log_info, columns=['Log_ID', 'Username', 'Privilegios', 'Acción', 'Descripción', 'Timestamp'])
+                    variables = ['Username', 'Privilegios', 'Acción', 'Timestamp']
+                    dictFilters = {column: list(df[column].unique()) for column in variables}
 
                     st.info('Filtros disponibles para los logs:')
 
-                    
                     if 'filters' not in st.session_state:
                         st.session_state['filters'] = []
 
-                    if st.button("Agregar filtro"):
-                        # Agregar un nuevo filtro
-                        st.session_state['filters'].append({})
+                    # Limitar la adición de filtros duplicados
+                    available_filters = [f for f in variables if f not in [list(d.keys())[0] for d in st.session_state['filters']]]
+
+                    if available_filters:
+                        if st.button("Agregar filtro"):
+                            # Agregar un nuevo filtro solo si hay filtros disponibles
+                            st.session_state['filters'].append({})
 
                     for i, filter_dict in enumerate(st.session_state['filters']):
-                        with st.container():
-                            key = st.selectbox(
-                                'Seleccione la clave del filtro:',
-                                options=list(dictFilters.keys()),
-                                key=f'key_select_{i}'
-                            )
-                            if key:
+                        col1, col2, col3 = st.columns([4, 4, 1])
+                        if not filter_dict:
+                            with col1:
+                                key = st.selectbox(
+                                    'Seleccione la clave del filtro:',
+                                    options=available_filters,
+                                    key=f'key_select_{i}'
+                                )
+                                st.session_state['filters'][i] = {key: None}
+                                st.rerun()
+                        else:
+                            key = list(filter_dict.keys())[0]
+                            with col1:
+                                st.selectbox(
+                                    'Seleccione la clave del filtro:',
+                                    options=[key],
+                                    key=f'key_fixed_{i}'
+                                )
+                            with col2:
                                 if key == 'Timestamp':
                                     # Rango de fechas para Timestamp
                                     date_range = st.date_input(
@@ -87,6 +96,16 @@ def ver_logs():
                                         key=f'value_select_{i}'
                                     )
                                     st.session_state['filters'][i] = {key: value}
+                            with col3:
+                                # Botón para eliminar el filtro
+                                if st.button('x', key=f'remove_filter_{i}'):
+                                    st.session_state['filters'].pop(i)
+                                    st.rerun()
+
+                    if st.session_state['filters']:
+                        if st.button("Eliminar todos los filtros"):
+                            st.session_state['filters'] = []
+                            st.rerun()
 
                     # Aplicar filtros al DataFrame
                     if st.button("Aplicar filtros"):
@@ -103,12 +122,12 @@ def ver_logs():
                         # Mostrar DataFrame filtrado
                         st.write(df_filtered)
 
-                except Exception as e:
-                    st.error("Ocurrió un error al recuperar los logs: " + str(e))
-                finally:
-                    session.close()
+            except Exception as e:
+                st.error("Ocurrió un error al recuperar los logs: " + str(e))
+            finally:
+                session.close()
 
-
-    # En teoria esto jamas se ve (ya que en menu.py se establece un filtro)
+    # En teoría esto jamás se ve (ya que en menu.py se establece un filtro)
     else:
         st.error('Favor de iniciar sesión para acceder a esta página.')
+
