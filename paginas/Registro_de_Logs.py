54303ae9393b4ac29bc69d487e02eb96c8f4e9ad
axl2301 <126207147+axl2301@users.noreply.github.com>
2024-06-03 14:51:29 -0600

    feat: changed filter selection to sidebar

--- paginas/Registro_de_Logs.py
+++ paginas/Registro_de_Logs.py
@@ -17,14 +17,13 @@ def ver_logs():
                 ### Funcionalidades:
 
                 - **Filtrar por Username**: Puedes seleccionar un usuario específico para ver únicamente sus actividades registradas.
-                - **Filtrar por Privilegios**: Filtra las actividades según los privilegios del usuario, como Admin, User o Colaborador.
-                - **Filtrar por Acción**: Selecciona un tipo de acción específica para ver todas las instancias en las que se realizó esa acción.
                 - **Filtrar por Rango de Fechas**: Selecciona un rango de fechas para ver las actividades registradas durante ese período.
-
+                - **Filtrar por Acción**: Selecciona un tipo de acción específica para ver todas las instancias en las que se realizó esa acción (Consulta de Migrante, Registro de Migrante o Manejo de Usuarios).
+                
                 ### Cómo Usar los Filtros:
 
                 1. Haz clic en el botón "Agregar filtro" para añadir un nuevo filtro.
-                2. Selecciona la clave del filtro (Username, Privilegios, Acción, Timestamp).
+                2. Selecciona la clave del filtro (Username, Timestamp, Acción).
                 3. Dependiendo de la clave seleccionada, elige el valor específico o el rango de fechas.
                 4. Repite los pasos anteriores para agregar múltiples filtros.
                 5. Haz clic en el botón "Aplicar filtros" para ver los registros filtrados.
@@ -44,10 +43,9 @@ def ver_logs():
                     log_info = [[log.id, log.user_name, log.user_type, log.action, log.description, log.timestamp] for log in logs]
                     # Crear un DataFrame con pandas y especificar los nombres de las columnas
                     df = pd.DataFrame(log_info, columns=['Log_ID', 'Username', 'Privilegios', 'Acción', 'Descripción', 'Timestamp'])
-                    variables = ['Username', 'Privilegios', 'Acción', 'Timestamp']
-                    dictFilters = {column: list(df[column].unique()) for column in variables}
+                    variables = ['Username', 'Timestamp', 'Acción']
 
-                    st.info('Filtros disponibles para los logs:')
+                    st.sidebar.info('Filtros disponibles para los logs:')
 
                     if 'filters' not in st.session_state:
                         st.session_state['filters'] = []
@@ -56,62 +54,61 @@ def ver_logs():
                     available_filters = [f for f in variables if f not in [list(d.keys())[0] for d in st.session_state['filters']]]
 
                     if available_filters:
-                        if st.button("Agregar filtro"):
+                        if st.sidebar.button("Agregar filtro"):
                             # Agregar un nuevo filtro solo si hay filtros disponibles
                             st.session_state['filters'].append({})
 
                     for i, filter_dict in enumerate(st.session_state['filters']):
-                        col1, col2, col3 = st.columns([4, 4, 1])
                         if not filter_dict:
-                            with col1:
-                                key = st.selectbox(
-                                    'Seleccione la clave del filtro:',
-                                    options=available_filters,
-                                    key=f'key_select_{i}'
-                                )
-                                st.session_state['filters'][i] = {key: None}
-                                st.rerun()
+                            key = st.sidebar.selectbox(
+                                'Clave del filtro:',
+                                options=available_filters,
+                                key=f'key_select_{i}'
+                            )
+                            st.session_state['filters'][i] = {key: None}
+                            st.rerun()
                         else:
                             key = list(filter_dict.keys())[0]
-                            with col1:
-                                st.selectbox(
-                                    'Seleccione la clave del filtro:',
-                                    options=[key],
-                                    key=f'key_fixed_{i}'
+                            st.sidebar.selectbox(
+                                'Clave del filtro:',
+                                options=[key],
+                                key=f'key_fixed_{i}'
+                            )
+                            if key == 'Timestamp':
+                                # Rango de fechas para Timestamp usando un slider con fechas
+                                min_date = pd.to_datetime(df[key]).min().date()
+                                max_date = pd.to_datetime(df[key]).max().date()
+                                start_date, end_date = st.sidebar.slider(
+                                    "Seleccione el rango de fechas:",
+                                    min_value=min_date,
+                                    max_value=max_date,
+                                    value=(min_date, max_date),
+                                    format="YYYY/MM/DD",
+                                    key=f'date_range_{i}'
                                 )
-                            with col2:
-                                if key == 'Timestamp':
-                                    # Rango de fechas para Timestamp
-                                    date_range = st.date_input(
-                                        "Seleccione el rango de fechas:",
-                                        value=(pd.to_datetime(df[key]).min().date(), pd.to_datetime(df[key]).max().date()),
-                                        key=f'date_range_{i}'
-                                    )
-                                    st.session_state['filters'][i] = {key: date_range}
-                                else:
-                                    # Valores para los otros campos
-                                    value = st.selectbox(
-                                        'Seleccione el valor:',
-                                        options=pd.unique(df[key].dropna()),
-                                        key=f'value_select_{i}'
-                                    )
-                                    st.session_state['filters'][i] = {key: value}
-                            with col3:
-                                # Botón para eliminar el filtro
-                                if st.button('x', key=f'remove_filter_{i}'):
-                                    st.session_state['filters'].pop(i)
-                                    st.rerun()
+                                st.session_state['filters'][i] = {key: (start_date, end_date)}
+                            else:
+                                # Valores para los otros campos
+                                value = st.sidebar.selectbox(
+                                    'Seleccione el valor:',
+                                    options=['Todos'] + list(pd.unique(df[key].dropna())),
+                                    key=f'value_select_{i}'
+                                )
+                                st.session_state['filters'][i] = {key: value if value != 'Todos' else None}
+                            if st.sidebar.button('x', key=f'remove_filter_{i}'):
+                                st.session_state['filters'].pop(i)
+                                st.rerun()
 
                     if st.session_state['filters']:
-                        if st.button("Eliminar todos los filtros"):
+                        if st.sidebar.button("Eliminar todos los filtros"):
                             st.session_state['filters'] = []
                             st.rerun()
 
                     # Aplicar filtros al DataFrame
-                    if st.button("Aplicar filtros"):
-                        df_filtered = df.copy()
-                        for filter_dict in st.session_state['filters']:
-                            for k, v in filter_dict.items():
+                    df_filtered = df.copy()
+                    for filter_dict in st.session_state['filters']:
+                        for k, v in filter_dict.items():
+                            if v is not None:
                                 if k == 'Timestamp':
                                     df_filtered['Date'] = pd.to_datetime(df_filtered[k]).dt.date
                                     df_filtered = df_filtered[(df_filtered['Date'] >= v[0]) & (df_filtered['Date'] <= v[1])]
@@ -119,8 +116,8 @@ def ver_logs():
                                 else:
                                     df_filtered = df_filtered[df_filtered[k] == v]
 
-                        # Mostrar DataFrame filtrado
-                        st.write(df_filtered)
+                    # Mostrar DataFrame filtrado
+                    st.write(df_filtered)
 
             except Exception as e:
                 st.error("Ocurrió un error al recuperar los logs: " + str(e))
