e138d37f369b5a71902a68e9bafb8813bc049d41
axl2301 <126207147+axl2301@users.noreply.github.com>
2024-06-03 14:51:09 -0600

    feat: added Bernardo's visualization

--- paginas/Visualizar_datos.py
+++ paginas/Visualizar_datos.py
@@ -4,10 +4,9 @@ import streamlit as st
 import base64
 from dotenv import load_dotenv
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
-import matplotlib.pyplot as plt
-import seaborn as sns
-import math
 import pandas as pd
+import plotly.express as px
+
 
 # our decrypt data function
 from back.encrypt import admin_decrypt_page
@@ -33,19 +32,13 @@ def visualizar_datos_page():
                 data = admin_decrypt_page(PRIVATE_KEY)
                 df = pd.json_normalize(data, sep='_')
                 df = df[df['current_member'] == 'si']
-                st.info('Cantidad de migrantes en Casa Monarca')
-                st.markdown(f"""
-                <div style='background-color:#000000; border: 1px solid #dcdcdc; padding: 5px; border-radius: 5px; text-align: center;'>
-                    <h2 style='font-size: 48px;'>{len(df)} </h2>
-                </div>
-                """, unsafe_allow_html=True)
-                st.write("")
-                st.info('Filtros para dashboards')
-                dictFilters = {}
+    
+                st.sidebar.info(f"Cantidad de migrantes actualmente en Casa Monarca: *{len(df)}*")
+
                 variables = [
                     "arrival_date", "type", "gender", "age", "country_of_origin", "civil_status",
                     "has_children", "children_traveling", "can_return_to_country", "access_to_casa_monarca",
-                    "services_provided", "assigned_dormitory", "date_left_origin", "traveling_alone_accompanied",
+                    "assigned_dormitory", "date_left_origin", "traveling_alone_accompanied",
                     "who_accompanied", "abuse_during_travel", "abuse_in_mexico", "paid_guide", "amount_paid",
                     "date_entered_mexico", "entry_point_mexico", "final_destination", "destination_monterrey",
                     "support_network_monterrey", "tried_enter_us", "support_network_us", "been_in_migration_station",
@@ -54,123 +47,51 @@ def visualizar_datos_page():
                     "is_pregnant", "months_pregnant", "has_prenatal_care", "can_read_write", "last_grade_study",
                     "languages_spoken", "other_language"
                 ]
-                dictFilters = {}
-                for column in df.columns.tolist():
-                    if column in variables:
-                        dictFilters[column] = list(df[column].unique())
 
-                if 'filters' not in st.session_state:
-                    st.session_state['filters'] = []
-
-                if st.button("Agregar filtro"):
-                    # Add a new filter slot
-                    st.session_state['filters'].append({})
-
-                for i, filter_dict in enumerate(st.session_state['filters']):
-                    with st.container():
-                        key = st.selectbox(
-                            'Seleccione la clave del filtro:',
-                            options=list(dictFilters.keys()),
-                            key=f'key_select_{i}'
-                        )
-                        if key:
-                            # Determine the type of data in the column
+                filters = {}
+                for key in variables:
+                    if key in df.columns:
+                        unique_vals = df[key].dropna().unique()
+                        if len(unique_vals) > 1:
                             if df[key].dtype == 'int64':
-                                # It's an integer, use a slider
-                                value_range = st.slider(
-                                    "Seleccione el rango de valores:",
-                                    min_value=int(df[key].min()),
-                                    max_value=int(df[key].max()),
-                                    value=(int(df[key].min()), int(df[key].max())),
-                                    key=f'range_slider_{i}'
-                                )
-                                # Update the dictionary to filter between this range
-                                st.session_state['filters'][i] = {key: value_range}
-                            elif isinstance(df[key].iloc[0], datetime.date):
-                                # It's a datetime, use a date input
-                                date_range = st.slider(
-                                    "Seleccione el rango de valores:",
-                                    min_value=(df[key].min()),
-                                    max_value=(df[key].max()),
-                                    value=((df[key].min()), (df[key].max())),
-                                    key=f'range_slider_{i}'
-                                )
-                                st.session_state['filters'][i] = {key: date_range}
+                                min_val, max_val = int(df[key].min()), int(df[key].max())
+                                value_range = st.sidebar.slider(f"{key} (rango)", min_val, max_val, (min_val, max_val))
+                                filters[key] = (df[key] >= value_range[0]) & (df[key] <= value_range[1])
+                            elif pd.api.types.is_datetime64_any_dtype(df[key]):
+                                min_val, max_val = df[key].min(), df[key].max()
+                                date_range = st.sidebar.date_input(f"{key} (rango)", [min_val, max_val])
+                                filters[key] = (df[key] >= date_range[0]) & (df[key] <= date_range[1])
                             else:
-                                # It's categorical or other, use a selectbox
-                                value = st.selectbox(
-                                    'Seleccione el valor:',
-                                    options=pd.unique(df[key].dropna()),
-                                    key=f'value_select_{i}'
-                                )
-                                st.session_state['filters'][i] = {key: value}
+                                value = st.sidebar.selectbox(f"{key}", ['Todos'] + list(unique_vals))
+                                if value != 'Todos':
+                                    filters[key] = (df[key] == value)
+                        elif len(unique_vals) == 1:
+                            st.sidebar.markdown(f"{key} tiene un único valor: {unique_vals[0]} (no se necesita filtro)")
 
-                # Apply filters to the dataframe
-                if st.button("Aplicar filtros"):
-                    df_filtered = df.copy()
-                    for filter_dict in st.session_state['filters']:
-                        for k, v in filter_dict.items():
-                            if isinstance(v, tuple) and isinstance(v[0], int):
-                                df_filtered = df_filtered[(df_filtered[k] >= v[0]) & (df_filtered[k] <= v[1])]
-                            elif isinstance(v, tuple) and isinstance(v[0], pd.Timestamp):
-                                df_filtered = df_filtered[(df_filtered[k] >= v[0]) & (df_filtered[k] <= v[1])]
-                            else:
-                                df_filtered = df_filtered[df_filtered[k] == v]
-                    # Display filtered dataframe
-                    df = df_filtered
+                if filters:
+                    for key, filter_condition in filters.items():
+                        df = df[filter_condition]
 
-                # After the line 'st.write(df_filtered)'
-                # Define the number of columns in the grid
-                num_columns = 3  # You can change this number based on your preference or screen size
-                print(plt.style.available)
-                plt.style.use('seaborn-v0_8-darkgrid')
-                plt.rcParams['axes.facecolor'] = 'none'  # Dark grey background for the axes
-                plt.rcParams['figure.facecolor'] = 'none'  # Darker grey background for the figure
-                plt.rcParams['axes.edgecolor'] = 'white'  # White edges to better delimit the plots
-                plt.rcParams['text.color'] = 'white'  # White text for better contrast
-                plt.rcParams['axes.labelcolor'] = 'white'
-                plt.rcParams['xtick.color'] = 'white'
-                plt.rcParams['ytick.color'] = 'white'
+                # Crear dos columnas para visualizar las gráficas
+                cols = st.columns(2)
+                col_index = 0
 
-                # Assume df is your DataFrame, also predefined
-                figures = []  # This will hold the figures to display later
+                # Colores sólidos para las gráficas
+                color_crimson = ["crimson"]
 
                 for variable in variables:
                     if variable in df.columns:
-                        fig, ax = plt.subplots(figsize=(10, 6))  # Larger figure size for better readability
-                        if df[variable].dtype == 'int64':
-                            # Plotting for integer variables with a nicer palette and edges
-                            sns.histplot(df[variable].dropna(), kde=True, color='skyblue', edgecolor='black', ax=ax)
-                            ax.set_title(f'Distribution of {variable}', fontsize=16)
-                        elif isinstance(df[variable].iloc[0], datetime.date):
-                            # Plotting for date variables with a line plot
-                            date_counts = df[variable].dropna().value_counts().sort_index()
-                            date_counts.plot(kind='line', color='green', ax=ax)
-                            ax.set_title(f'Time Series of {variable}', fontsize=16)
-                            ax.set_xlabel('Date', fontsize=12)
-                            ax.set_ylabel('Frequency', fontsize=12)
+                        if df[variable].dtype in ['int64', pd.api.types.is_datetime64_any_dtype(df[variable])]:
+                            fig = px.histogram(df, x=variable, title=f'Distribución de {variable}', color_discrete_sequence=color_crimson)
                         else:
-                            # Plotting for object variables with better visual handling of categories
-                            value_counts = df[variable].value_counts()
-                            sns.barplot(x=value_counts.index, y=value_counts.values, palette='viridis', ax=ax)
-                            ax.set_title(f'Frequency of {variable}', fontsize=16)
-                            ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
-                        ax.set_xlabel(ax.get_xlabel(), fontsize=12)
-                        ax.set_ylabel(ax.get_ylabel(), fontsize=12)
-                        ax.tick_params(labelsize=10)  # Smaller label size
-                        figures.append(fig)  # Append the figure to the list
+                            # Contando los valores correctamente para categorías
+                            data_to_plot = df[variable].value_counts().reset_index()
+                            data_to_plot.columns = ['category', 'count']
+                            fig = px.bar(data_to_plot, x='category', y='count', title=f'Frecuencia de {variable}', color_discrete_sequence=color_crimson)
 
-                # Assume you have a Streamlit setup with num_columns defined
-                num_rows = math.ceil(len(figures) / num_columns)  # Calculate the number of rows needed
-
-                # Display the figures in a grid
-                for i in range(num_rows):
-                    cols = st.columns(num_columns)  # Create a row of columns
-                    for j in range(num_columns):
-                        idx = i * num_columns + j
-                        if idx < len(figures):
-                            with cols[j]:
-                                st.pyplot(figures[idx])
+                        if fig:
+                            cols[col_index].plotly_chart(fig, use_container_width=True)
+                            col_index = (col_index + 1) % 2  # Alternar entre las dos columnas
 
 
     else:
